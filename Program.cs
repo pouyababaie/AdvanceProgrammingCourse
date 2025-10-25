28aeb1779531233d0534713c491091d4897e1630
pouyababaie <pouyababaie123@gmail.com>
2025-10-25 23:57:55 +0330

    Task 10

--- Program.cs
+++ Program.cs
@@ -6,28 +6,74 @@
         {
             Console.Clear();
 
-            Console.WriteLine("Eneter the Centigerad Number \n");
-            string input = Console.ReadLine();
+            Console.WriteLine("Eneter a Number \n");
+            string input1 = Console.ReadLine();
 
-            int inputNumber = int.Parse(input);
+            Console.WriteLine("Eneter a Number \n");
+            string input2 = Console.ReadLine();
 
-            Console.WriteLine($"The Reveresed Number is : {reverseNumWithwhile(inputNumber)}" );
+            int input1Number = int.Parse(input1);
 
-            Console.ReadLine();
-        }
+            int input2Number = int.Parse(input2);
 
-        public static int reverseNumWithwhile(int number)
-        {
-            int reverse = 0;
-            while (number > 0)
+
+            Console.WriteLine("Wich Operator are you using ?");
+            Console.WriteLine("1) Change Variable With Temporary Variable");
+            Console.WriteLine("2) Change Variable With XOR");
+            Console.WriteLine("3) Change Variable With Arithmetic");
+
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
             {
-                int mod = number % 10;
-                reverse *= 10;
-                reverse += mod;
+                case "1":
+                    ChangeVariableWithTempVariable(input1Number, input2Number);
+                    break;
 
-                number /= 10;
+                case "2":
+                    ChangeVariableWithXOR(input1Number, input2Number);
+
+                    break;
+
+                case "3":
+                    ChangeVariableWithArithmetic(input1Number, input2Number);
+                    break;
             }
-            return reverse;
         }
+
+
+
+        public static void ChangeVariableWithTempVariable(int a, int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+
+            Console.WriteLine($"Value of the A : {a} \n");
+            Console.WriteLine($"Value of the B : {b}");
+        }
+
+        public static void ChangeVariableWithXOR(int a, int b)
+        {
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            Console.WriteLine($"Value of the A : {a} \n");
+            Console.WriteLine($"Value of the B : {b}");
+        }
+
+        public static void ChangeVariableWithArithmetic(int a, int b)
+        {
+
+            a = a ^ b;
+            b = a ^ b;
+            a = a ^ b;
+
+            Console.WriteLine($"Value of the A : {a} \n");
+            Console.WriteLine($"Value of the B : {b}");
+        }
+
     }
 }
