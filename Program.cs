ff567cd706b25bac2a032edc920f906e7d736e14
pouyababaie <pouyababaie123@gmail.com>
2025-10-25 23:34:41 +0330

    Task 8

--- Program.cs
+++ Program.cs
@@ -6,25 +6,19 @@
         {
             Console.Clear();
 
-            Console.WriteLine("Eneter the First Number \n");
-            string first = Console.ReadLine();
+            Console.WriteLine("Eneter the Centigerad Number \n");
+            string centigerad = Console.ReadLine();
 
-            Console.WriteLine("Eneter the Second Number \n");
-            string second = Console.ReadLine();
 
-            Console.WriteLine("Eneter the Third Number \n");
-            string third = Console.ReadLine();
-
-            bool isNumber = int.TryParse(first, out int value) && int.TryParse(second, out int value2) && int.TryParse(third, out int value3);
+            bool isNumber = double.TryParse(centigerad, out double value);
 
 
 
             if (isNumber)
             {
-                int firstNumber = int.Parse(first);
-                int secondNumber = int.Parse(second);
-                int thirdNumber = int.Parse(third);
-                Console.WriteLine($"The Average of the inputs is : {(firstNumber + secondNumber + thirdNumber) / 3}");
+                double centigeradNumber = double.Parse(centigerad);
+
+                Console.WriteLine($"The Average of the inputs is : {(centigeradNumber * 1.8) + 32}");
 
             }
 
