0e178eadddf8eeed695d9cb73023e6bed96cac43
pouyababaie <pouyababaie123@gmail.com>
2025-10-25 23:17:45 +0330

    Task 5

--- Program.cs
+++ Program.cs
@@ -4,88 +4,33 @@
     {
         static void Main(string[] args)
         {
+            Console.Clear();
 
-            bool exit = false;
-            while (!exit)
+
+            Console.WriteLine("Rectangle Area Calculator \n");
+
+            Console.WriteLine("Eneter the X Axis ");
+            string XAxis = Console.ReadLine();
+
+            Console.WriteLine("Eneter the Y Axis ");
+            string YAxis = Console.ReadLine();
+
+            bool isNumber = int.TryParse(XAxis, out int value) && int.TryParse(YAxis, out int value2);
+
+
+            int XAxisNumber = int.Parse(XAxis);
+
+            int YAxisNumber = int.Parse(YAxis);
+
+            if (isNumber)
             {
+                Console.WriteLine($"The Area of the Rectangle is : {XAxisNumber * YAxisNumber}");
+                Console.WriteLine($"The Perimeter of the Rectangle is : {2 * (XAxisNumber + YAxisNumber)}");
 
-                ShowMenu();
-                string choice = Console.ReadLine();
-
-                var isNumeric = int.TryParse(choice, out _);
-
-                if (isNumeric)
-                {
-                    Console.WriteLine($"Your Choice is {choice} \n Entering Setting");
-
-                    switch (choice)
-                    {
-                        case "1":
-                            break;
-
-                        case "2":
-                            break;
-
-                        case "3":
-                            break;
-
-                        case "4":
-                            break;
-
-                        case "5":
-                            break;
-
-                        case "6":
-                            break;
-
-                        case "7":
-                            break;
-
-                        case "8":
-                            break;
-
-                        case "9":
-                            break;
-
-                        case "0":
-
-                            exit = true;
-                            Console.WriteLine("Exiting... Goodbye!");
-
-                            break;
-                    }
-
-                }
-                else
-                {
-                    Console.WriteLine($"Undefined Input {choice}");
-                    exit = true;
-                }
             }
 
-            static void ShowMenu()
-            {
-                Console.Clear();
-                Console.WriteLine("═══════════════════════════════════════════════════");
-                Console.WriteLine("          BOOK MANAGEMENT SYSTEM");
-                Console.WriteLine("═══════════════════════════════════════════════════");
-                Console.WriteLine("1)---------------->  Add New Book");
-                Console.WriteLine("2)---------------->  Search the books by name and show details");
-                Console.WriteLine("3)---------------->  Show all the books");
-                Console.WriteLine("4)---------------->  Remove the book by ISBN");
-                Console.WriteLine("5)---------------->  Show the most expensive book");
-                Console.WriteLine("6)---------------->  Show the cheapest book");
-                Console.WriteLine("7)---------------->  Show number of books");
-                Console.WriteLine("8)---------------->  Show the books by price(Sort by price)");
-                Console.WriteLine("9)---------------->  Show the books between two prices");
-                Console.WriteLine("0)---------------->  Exit");
-                Console.WriteLine("═══════════════════════════════════════════════════");
-                Console.Write("Please enter a number: ");
-            }
-
+            Console.ReadLine();
         }
-
-
 
     }
 }
